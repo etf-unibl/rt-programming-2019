bfb58c5c9b6526ea07d8249e4ada949ef8602b2f
Mladen Knezic <mladen.knezic@gmail.com>
2019-03-14 14:49:29 -0700

    Fixed some examples

--- lectures/week-02/heap/malloc-4.c
+++ lectures/week-02/heap/malloc-4.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <malloc.h>
 
 int main()
 {
+	
+	// disable mmap allocation
+	//mallopt(M_MMAP_MAX, 0);
+	// redefine mmap threshold
+	//mallopt(M_MMAP_THRESHOLD, 150*1024);
 
 	write(1, "BEFORE MALLOC\n", 14);
 	malloc(150000);
